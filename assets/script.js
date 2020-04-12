3068642d4a7fdb2fce88142bb4031a0cb26ac1c3
sophiecheav <sophie.cheavseang@gmail.com>
2020-04-12 22:04:57 +0200

    connect form on header
    + change logo + test script for menu

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+function hideShowMenu() {
+	var x = document.querySelector('.menu');
+  if (x.style.display == "none")
+  { x.style.display = "block";
+	}
+    else
+  { x.style.display = "none";
+	}
+}
+
+document.querySelector('.menu').style.display='none';
